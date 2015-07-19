7dad58acf2bf0388297ce18c4efbd34e5601829c
Chen Wang <contact@chenwang.org>
2015-07-19 21:42:44 +0100

    added parsing script for SportSystems.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a toolbox for scripts useful in supporting Serpentine Running Club.
---
+++ SportSystemsDataParser.groovy
@@ -0,0 +1,23 @@
+def root = new XmlSlurper().parse(new File(args[0]))
+def output = new File(args[1])
+output.write ''
+output << "position,guntime,chiptime,fname,lname,category,number,grade,club\r\n"
+def columns = [0, 1, 6, 2, 4, 5, 7]
+root.row.each { r ->
+	columns.each { c ->
+		if (c != 0) {
+			output << ','
+		}
+		if (c == 2) {
+			def v = r.cell[c].text()
+			def names = v.substring(v.indexOf('>') + 1, v.indexOf('</a>')).split()
+			output << names[0]
+			output << ','
+			output << names[1]
+		}
+		else {
+			output << r.cell[c].text()
+		}
+	}
+	output << ",Serpentine\r\n"
+}
